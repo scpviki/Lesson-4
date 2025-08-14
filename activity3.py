396662b94c1d737f210ca495a69aa0e4e015f87c
scpviki <arun.letmein@gmail.com>
2025-08-14 11:57:42 +0400

    new code3

---
+++ activity3.py
@@ -0,0 +1,7 @@
+math=int(input("enter your marks "))
+science=int(input("enter your marks "))
+english=int(input("enter your marks "))
+ICT=int(input("enter your marks "))
+sum=math+science+english+ICT
+percentage=(sum/400)*100      
+print(f"percentage is {percentage}")
